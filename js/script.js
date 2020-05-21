a2d46b6ec1b65dc53d3cc9895c4c21fdb8ab6c11
Muia23 <64227473+Muia23@users.noreply.github.com>
2020-05-21 17:58:33 +0300

    add quantityChanged event
    used to adjust the quantity and update the the total

--- js/script.js
+++ js/script.js
@@ -37,3 +37,10 @@ function removeCartItem(event) {
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 }
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
